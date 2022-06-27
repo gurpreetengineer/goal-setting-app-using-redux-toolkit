e5e9aac31e62ee5782ce03b2312f658975f18de3
Garry Bamrah <gurpreet.reactdev@gmail.com>
2022-06-27 19:11:55 +0530

    Login and register pages partially done

--- src/CustomRoutes.jsx
+++ src/CustomRoutes.jsx
@@ -1,14 +1,15 @@
 import React, { Fragment, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Register from './pages/Register';
 
 const Dashboard = lazy(() => import('./pages/Dashboard'));
-// const Register = lazy(() => import('./pages/Register'));
+const Register = lazy(() => import('./pages/Register'));
 const Login = lazy(() => import('./pages/Login'));
+const Header = lazy(() => import('./components/Header'));
 
 const CustomRoutes = () => {
   return (
     <Fragment>
+      <Header />
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/register" element={<Register />} />
