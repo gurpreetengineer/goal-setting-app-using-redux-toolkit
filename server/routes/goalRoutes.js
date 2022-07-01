bfbfc7c1571f682ea571d7f79673730298e8ec5a
Garry Bamrah <gurpreet.reactdev@gmail.com>
2022-07-01 21:58:00 +0530

    nothing much

--- server/routes/goalRoutes.js
+++ server/routes/goalRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalsController')
+const { 
+  getGoals, 
+  setGoal, 
+  updateGoal, 
+  deleteGoal 
+} = require('../controllers/goalsController')
 
 router.route('/').get(getGoals).post(setGoal)
 router.route('/:id').put(getGoals).delete(deleteGoal)
