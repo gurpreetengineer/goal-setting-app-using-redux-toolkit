3b1efd87d99532c91fd1755bf001b5a388156c27
Garry Bamrah <gurpreet.reactdev@gmail.com>
2022-07-07 20:05:54 +0530

    /register and /login APIs improved

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,15 +1,7 @@
-
-/**
- * @desc    Login User
- * @route   POST /api/users/login
- * @access  Public
- * @params  req, res
- */
-const loginUser = (req, res) => {
-  res.status(200).send({
-    msg: 'Provide me the login users info'
-  })
-}
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
 
 /**
  * @desc    Register User
@@ -17,11 +9,71 @@ const loginUser = (req, res) => {
  * @access  Public
  * @params  req, res
  */
-const registerUser = (req, res) => {
-  res.status(200).send({
-    msg: 'Please register the user'
+ const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Field(s) are empty');
+  }
+
+  // Check if user already exists in the database, remember (here) await is very important.
+  // Without it a circular object will be returned which will always implies as a true value.
+  const userExists = await User.findOne({ email });
+  console.log(userExists)
+  if (userExists) {
+    res.status(400)
+    throw new Error(`User already exists ${userExists}`)
+  }
+
+  // Hashing of the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // User creation with hashed password
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
   })
-}
+
+  if (user) {
+    res.status(201).json({
+      msg: 'User created',
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      password: hashedPassword,
+    })
+  } else {
+    res.status(400);
+    throw new Error('Invalid user. Please check and try again');
+  }
+})
+
+/**
+ * @desc    Login User
+ * @route   POST /api/users/login
+ * @access  Public
+ * @params  req, res
+ */
+const loginUser = asyncHandler(async (req, res) => {
+  const {email, password} = req.body
+
+  const user = await User.findOne({email})
+
+  if(user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      msg: "Login successful",
+      _id: user.id,
+      name: user.name,
+      email: user.email
+    });
+  } else {
+    res.status(400)
+    throw new Error('Invalid Credentials');
+  }
+})
 
 /**
  * @desc    Fetch User information
@@ -29,11 +81,11 @@ const registerUser = (req, res) => {
  * @access  Public
  * @params  req, res
  */
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
   res.status(200).send({
     msg: 'Here\'s the user\'s information.'
   })
-}
+})
 
 module.exports = {
   loginUser,
